1d9719877919f0094a9becc4ae3050de839c6b19
Michael Taylor <taylor_mikey77@yahoo.com>
2019-08-26 17:57:10 -0500

    Update floodfill.js

---
+++ week4/floodfill.js
@@ -0,0 +1,18 @@
+// Flood Fill
+// Most graphical “paint” applications have a ‘paintcan fill’ function that floods part of an image with a certain color. We change the image as if we painted a canvas: a two-dimensional array of integers, where each integer represents a color for that pixel. The canvas Array.length is the Y dimension of our canvas; each spot in the canvas array is a row in our image, with a length equal to our canvas’ X dimension. You are given a canvas (2-dimensional array of integers), starting coordinate (2-element array), and the color to flood (integer value). Build floodFill(canvas2D,startXY,newColor)! Replace a pixel’s color value only if it is the same color as the origin coordinate and is directly adjacent via X or Y to another pixel you will change.
+
+// exampleCanvas = [[3,2,3,4,3],
+//                  [2,3,3,4,0],
+//                  [7,3,3,5,3],
+//                  [6,5,3,4,1],
+//                  [1,2,3,3,3]]
+
+// floodFill(exampleCanvas, [2,2], 1)
+
+//     --> [[3,2,1,4,3],
+//          [2,3,1,4,0],
+//          [7,1,1,5,3],
+//          [6,5,1,4,1],
+//          [1,2,1,1,1]]
+
+// Note: diagonally related pixels are not considered adjacent.
