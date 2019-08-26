15e712161731acd5428751f888e946a391d2401e
Michael Taylor <taylor_mikey77@yahoo.com>
2019-08-26 16:23:06 -0500

    Fun with FloodFill

--- week4/recursion.js
+++ week4/recursion.js
@@ -17,36 +17,79 @@
 
 // Note: diagonally related pixels are not considered adjacent.
 
-function floodFill(canvas, coord, color) {
-    // if (canvas[0][0] !== canvas[coord[0]][coord[1]]) {
-    //     return "Should Not Change This Color"
-    // }
-    // else{
-    //     canvas[coord[0]][coord[1]] = color;
-    // }
-    let ogVal=canvas[coord[0]][coord[1]]
-    canvas[coord[0]][coord[1]] = color;
+// function floodFill(canvas, coord, color) {
+//     // if (canvas[0][0] !== canvas[coord[0]][coord[1]]) {
+//     //     return "Should Not Change This Color"
+//     // }
+//     // else{
+//     //     canvas[coord[0]][coord[1]] = color;
+//     // }
+//     let ogVal=canvas[coord[0]][coord[1]]
+//     canvas[coord[0]][coord[1]] = color;
+//     let newCoord = coord;
+//     newCoord[1] -= 1;
+//     if(newCoord[0] < canvas.length || newCoord[1] < canvas[0].length){
+//         if (canvas[newCoord[0]][newCoord[1]] == ogVal){
+//             floodFill(canvas, newCoord, color);
+//         }
+//     }
+//     newCoord = coord;
+//     newCoord[1] += 1;
+//     if(newCoord[0] < canvas.length || newCoord[1] < canvas[0].length){
+//         if (canvas[newCoord[0]][newCoord[1]] == ogVal){
+//             floodFill(canvas, newCoord, color);
+//         }
+//     }
+//     newCoord = coord;
+//     newCoord[0] += 1;
+//     if(newCoord[0] < canvas.length || newCoord[1] < canvas[0].length){
+//         console.log(newCoord[0], canvas.length)
+//         console.log(newCoord[1], canvas[0].length)
+//         if (canvas[newCoord[0]][newCoord[1]] == ogVal){
+//             floodFill(canvas, newCoord, color);
+//         }
+//     }
+//     newCoord = coord;
+//     newCoord[0] -= 1;
+//     if(newCoord[0] < canvas.length || newCoord[1] < canvas[0].length){
+//         if (canvas[newCoord[0]][newCoord[1]] == ogVal){
+//             floodFill(canvas, newCoord, color);
+//         }
+//     }
+//     return canvas
+// }
+
+let count=0
+
+function floodFill(canvas, target, coord, color){
+    count++
+    if (coord[0] > canvas.length - 1 || coord[0]<0){
+        return
+    }
+    if (coord[1] > canvas[0].length - 1 || coord[1]<0){
+        return
+    }
+    if (canvas[coord[0]][coord[1]] != target){
+        return
+    }
+    canvas[coord[0]][coord[1]] = color
     let newCoord = coord;
-    newCoord[1] -= 1;
-    if (canvas[newCoord[0]][newCoord[1]] == ogVal){
-        floodFill(canvas, newCoord, color);
-    }
+    newCoord[0] -= 1
+    console.log("DOWN", newCoord)
+    floodFill(canvas, target, newCoord, color)
     newCoord = coord;
-    newCoord[1] += 1;
-    if (canvas[newCoord[0]][newCoord[1]] == ogVal){
-        floodFill(canvas, newCoord, color);
-    }
+    newCoord[0] += 1
+    console.log("UP", newCoord)
+    floodFill(canvas, target, newCoord, color)
     newCoord = coord;
-    newCoord[0] += 1;
-    if (canvas[newCoord[0]][newCoord[1]] == ogVal){
-        floodFill(canvas, newCoord, color);
-    }
+    newCoord[1] -= 1
+    console.log("LEFT", newCoord)
+    floodFill(canvas, target, newCoord, color)
     newCoord = coord;
-    newCoord[0] -= 1;
-    if (canvas[newCoord[0]][newCoord[1]] == ogVal){
-        floodFill(canvas, newCoord, color);
-    }
-    return "Finished"
+    newCoord[1] += 1
+    console.log("RIGHT", newCoord)
+    floodFill(canvas, target, newCoord, color)
+    return canvas
 }
 
 let testCanvas = [[3, 2, 3, 4, 3],
@@ -61,4 +104,6 @@ console.log(floodFill([[3, 2, 3, 4, 3],
     [2, 3, 3, 4, 0], 
     [7, 3, 3, 5, 3], 
     [6, 5, 3, 4, 1], 
-    [1, 2, 3, 3, 3]], [2,2], 1))
+    [1, 2, 3, 3, 3]], 3, [2,2], 1))
+
+console.log(count)
