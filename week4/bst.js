999dcc97e7b8bdbe920ef58d7d1b80113fee0c54
Michael Taylor <taylor_mikey77@yahoo.com>
2019-08-28 15:57:41 -0500

    Update bst.js

--- week4/bst.js
+++ week4/bst.js
@@ -6,6 +6,28 @@ function BTNode(value) {
 function BST() {
     this.root = null;
     
-    this.add = function(value){
+    this.add = function(value, runner){
+        if(this.root == null){
+            this.root = new BTNode(value)
+            return this
+        }
+        else if(runner == null){
+            console.log(runner)
+            runner = new BTNode(value);
+            return this
+        } else {
+            if(value > runner.value){
+                let runner = runner.right
+                this.add(value, runner)
+            }
+            else if (value <= runner.value){
+                let runner = runner.left
+                this.add(value, runner)
+            }
+        }
     }
 }
+
+let searchTree = new BST()
+
+console.log(searchTree.add(4).add(7))
