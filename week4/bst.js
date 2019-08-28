628904e9b760f03732146c8d813bd7d104a56902
Michael Taylor <taylor_mikey77@yahoo.com>
2019-08-28 13:47:12 -0500

    Create bst.js

---
+++ week4/bst.js
@@ -0,0 +1,11 @@
+function BTNode(value) {
+    this.val = value;
+    this.left = null;
+    this.right = null;
+}
+function BST() {
+    this.root = null;
+    
+    this.add = function(value){
+    }
+}
